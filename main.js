504aa8a8c5c3f78ecffb5a7ebce842e378f3b08e
Kazimir84 <kav2007@ukr.net>
2021-08-24 14:22:42 +0300

    Fixed remarks in js file

--- main.js
+++ main.js
@@ -1,57 +1,127 @@
+// ============24.08.2021г.===================================
 class Clock {
-    constructor() {
+    constructor() { 
         this.clockTime = document.getElementById('clockTime');
-    }
+    };
     getTime() {
         return new Date();
     };
-    start() {
-        this.interval = setInterval(this.render.bind(this), 250);
-    };
-    stop() {
-        clearInterval(this.interval);
-    };
     render() {  
-        this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
-        this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();  
-        this.seconds = this.getTime().getSeconds() < 10 ? '0' + this.getTime().getSeconds() : this.getTime().getSeconds();         
-    
-        this.clockTime.innerHTML = this.hours + ":" + this.minutes + ":" + this.seconds;
+        if (this.clockTime.classList.contains('fullFormat')) {   
+            clockFullFormat.render();  
+        } else {
+            clockShortFormat.render(); 
+        };
     };
-
 };
-let clock = new Clock()
+let clock = new Clock();
 
 class СlockFullFormat extends Clock {
-    constructor() {
-        super();
-    }
     render() {
-        this.clockTime.innerHTML = this.getTime().getHours() + ":" + this.getTime().getMinutes() + ":" + this.getTime().getSeconds();          
-    };
-}
+        this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
+        this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();  
+        this.seconds = this.getTime().getSeconds() < 10 ? '0' + this.getTime().getSeconds() : this.getTime().getSeconds();  
 
-class СlockShortFormat extends Clock {
-    constructor() {
-        super();
+        this.clockTime.innerHTML = this.hours + ":" + this.minutes + ":" + this.seconds;          
     };
+};
+class СlockShortFormat extends Clock {
     render() {
-        this.clockTime.innerHTML = this.getTime().getHours() + ":" +this.getTime().getMinutes();      
+        this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
+        this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();          
+
+        this.clockTime.innerHTML = this.hours + ":" +this.minutes;      
     };
 };
 
 let clockFullFormat = new СlockFullFormat();
 let clockShortFormat = new СlockShortFormat();
 
-clockFullFormat.start();
+setInterval(() => {clock.render()}, 250);
 
 this.clockTime.addEventListener('click', () => {
-    this.clockTime.classList.toggle('fullFormat');
-    if (this.clockTime.classList.contains('fullFormat')) {   
-        clockFullFormat.start();
-        clockShortFormat.stop();
-    } else {
-        clockShortFormat.start();
-        clockFullFormat.stop();
-    };
-});
+        this.clockTime.classList.toggle('fullFormat');
+    });
+
+
+
+
+
+
+// ============22.08.2021г.===================================
+// class Clock {
+//     constructor() {
+//         this.clockTime = document.getElementById('clockTime');
+//     };
+//     getTime() {
+//         return new Date();
+//     };
+//     start() {
+//         this.render();
+//         this.interval = setInterval(this.render.bind(this), 250);
+//     };
+//     stop() {
+//         clearInterval(this.interval);
+//     };
+//     render() {  
+//         this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
+//         this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();  
+//         this.seconds = this.getTime().getSeconds() < 10 ? '0' + this.getTime().getSeconds() : this.getTime().getSeconds();         
+    
+//         this.clockTime.innerHTML = this.hours + ":" + this.minutes + ":" + this.seconds;
+//     };
+
+// };
+// let clock = new Clock();
+
+// class СlockFullFormat extends Clock {
+//     constructor() {
+//         super();
+//     };
+//     render() {
+//         this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
+//         this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();  
+//         this.seconds = this.getTime().getSeconds() < 10 ? '0' + this.getTime().getSeconds() : this.getTime().getSeconds();  
+
+//         this.clockTime.innerHTML = this.hours + ":" + this.minutes + ":" + this.seconds;          
+//     };
+// };
+// class СlockShortFormat extends Clock {
+//     constructor() {
+//         super();    
+//     };
+//     render() {
+//         this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
+//         this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();  
+//         this.seconds = this.getTime().getSeconds() < 10 ? '0' + this.getTime().getSeconds() : this.getTime().getSeconds();  
+
+//         this.clockTime.innerHTML = this.hours + ":" +this.minutes;      
+//     };
+// };
+
+// let clockFullFormat = new СlockFullFormat();
+// let clockShortFormat = new СlockShortFormat();
+
+// clockFullFormat.start();
+// // clockShortFormat.start();
+
+// class ChengeFormat extends Clock {
+//     constructor() {
+//         super();
+//     };
+//     format() {
+//         this.clockTime.addEventListener('click', () => {
+//             this.clockTime.classList.toggle('fullFormat');
+//             if (this.clockTime.classList.contains('fullFormat')) {   
+//                 clockFullFormat.start();
+//                 clockShortFormat.stop();
+//             } else {
+//                 clockShortFormat.start();
+//                 clockFullFormat.stop();
+//             };
+//         });
+//     };
+// };
+
+// let chengeFormat = new ChengeFormat();
+// chengeFormat.format();
